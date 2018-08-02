c16e30188e15b6c91ab1a4386a8c102070448f01
Marcin <marcin7848@gmail.com>
2018-08-02 22:28:17 +0200

    PersonController -> AccountController

---
+++ src/main/java/com/goode/controller/AccountController.java
@@ -0,0 +1,10 @@
+package com.goode.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/account")
+public class AccountController {
+
+}
