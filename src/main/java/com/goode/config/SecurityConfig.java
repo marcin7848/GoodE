9d0274b2921a96cd41b0a9edda4e013da80bb1ac
Marcin <marcin7848@gmail.com>
2018-08-05 23:57:12 +0200

    BCryptasswordEndocer impl

--- src/main/java/com/goode/config/SecurityConfig.java
+++ src/main/java/com/goode/config/SecurityConfig.java
@@ -2,12 +2,14 @@ package com.goode.config;
 
 import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -17,6 +19,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   @Autowired
   private DataSource dataSource;
 
+  @Bean
+  public BCryptPasswordEncoder bCryptPasswordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
   @Override
   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
@@ -24,7 +31,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         .usersByUsernameQuery("select username, password, enabled"
             + " from accounts where username=?")
         .authoritiesByUsernameQuery("select username, role"
-            + " from accounts where username=?");
+            + " from accounts where username=?").passwordEncoder(bCryptPasswordEncoder());
   }
 
   @Override
