0b17bb254ccd1b52334c2f6ec8339e25482b3e39
Marcin <marcin7848@gmail.com>
2018-08-17 20:24:00 +0200

    language enum added

---
+++ src/main/java/com/goode/Language.java
@@ -0,0 +1,22 @@
+package com.goode;
+
+public enum Language {
+
+  REGISTRATION_GOODE("Rejestracja GoodE", "Registration GoodE");
+
+  private String polish;
+  private String english;
+
+  Language(String polish, String english){
+    this.polish = polish;
+    this.english = english;
+  }
+
+  public String getString(){
+    boolean polish_active = true;
+
+    if(polish_active)
+      return this.polish;
+    return this.english;
+  }
+}
