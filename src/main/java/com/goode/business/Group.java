8c15cb3aee498f3e696540b9cc31d76f36761086
Marcin <marcin7848@gmail.com>
2018-08-31 21:18:03 +0200

    base for groups management

--- src/main/java/com/goode/business/Group.java
+++ src/main/java/com/goode/business/Group.java
@@ -4,6 +4,7 @@ import java.sql.Timestamp;
 import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -58,6 +59,6 @@ public class Group {
   @NotNull
   private Timestamp creationTime;
 
-  @OneToMany(mappedBy = "group")
+  @OneToMany(fetch = FetchType.LAZY, mappedBy = "group")
   private List<GroupMember> groupMembers;
 }
--- src/main/java/com/goode/controller/GroupController.java
+++ src/main/java/com/goode/controller/GroupController.java
@@ -1,15 +1,31 @@
 package com.goode.controller;
 
+import com.goode.business.Account;
 import com.goode.business.Group;
 import com.goode.service.GroupService;
+import java.security.Principal;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/group")
 public class GroupController extends BaseController<Group, GroupService> {
 
   @Autowired
   GroupService groupService;
+
+  @GetMapping
+  public ResponseEntity<?> addNew(Principal principal){
+
+    Account account = new Account();
+    account.setUsername(principal.getName());
+
+    return new ResponseEntity<>(account, HttpStatus.OK);
+  }
 
 
 }
