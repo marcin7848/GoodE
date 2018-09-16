b7e863653b926e6d35fb67c27254c45fff90fc55
Marcin <marcin7848@gmail.com>
2018-09-16 22:25:08 +0200

    resendActivationCode - validate Email from @Pattern

--- src/main/java/com/goode/validator/AccountValidator.java
+++ src/main/java/com/goode/validator/AccountValidator.java
@@ -4,9 +4,13 @@ import com.goode.ErrorCode;
 import com.goode.business.Account;
 
 import com.goode.repository.AccountRepository;
+import java.util.Set;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.validation.Errors;
 
 @Component
 public class AccountValidator extends BaseValidator  {
@@ -14,17 +18,19 @@ public class AccountValidator extends BaseValidator  {
   @Autowired
   private AccountRepository accountRepository;
 
-  public boolean validateEmail(String email){
-    return email.matches("^[a-zA-Z0-9._-]+@([a-zA-Z0-9-_]+\\.)+[a-zA-Z0-9-_]+$");
-  }
-
   public Account validateAccountActivation(String email, ErrorCode errorCode){
-    if (!validateEmail(email)) {
+    Account account = new Account();
+    account.setEmail(email);
+    ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+    Validator validator = factory.getValidator();
+    Set<ConstraintViolation<Account>> errors = validator.validateProperty(account, "email", Account.ValidationStepTwo.class);
+
+    if(!errors.isEmpty()) {
       errorCode.rejectValue("email", "validate.email.incorrect");
       return null;
     }
 
-    Account account = accountRepository.findAccountByEmail(email);
+    account = accountRepository.findAccountByEmail(email);
     if (account == null) {
       errorCode.rejectValue("email", "validate.email.incorrect");
       return null;
