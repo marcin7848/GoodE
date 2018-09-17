83618092583e0bf908774e921c48df4ae1b55ec9
Marcin <marcin7848@gmail.com>
2018-09-17 21:45:35 +0200

    poprawka stylizacji

--- src/main/java/com/goode/service/AccountService.java
+++ src/main/java/com/goode/service/AccountService.java
@@ -19,7 +19,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
-public class AccountService implements AccountServiceI, StandardizeService<Account>{
+public class AccountService implements AccountServiceI, StandardizeService<Account> {
 
   @Autowired
   private AccountRepository accountRepository;
@@ -33,19 +33,21 @@ public class AccountService implements AccountServiceI, StandardizeService<Accou
   @Autowired
   private ActivationCodeService activationCodeService;
 
-  public Account getAccountByPrincipal(Principal principal){
-    return accountRepository.findAccountByUsername(principal.getName()) ;
+  public Account getAccountByPrincipal(Principal principal) {
+    return accountRepository.findAccountByUsername(principal.getName());
   }
 
-  public Account getAccountById(int id_account){
+  public Account getAccountById(int id_account) {
     return accountRepository.findAccountById(id_account);
   }
 
   @Override
-  public Account addNew(Account account){
+  public Account addNew(Account account) {
 
-    if(!accountRepository.findByUsernameOrEmail(account.getUsername(), account.getEmail()).isEmpty())
+    if (!accountRepository.findByUsernameOrEmail(account.getUsername(), account.getEmail())
+        .isEmpty()) {
       return null;
+    }
 
     account.setEnabled(false);
     account.setAccessRole(accessRoleRepository.getAccessRoleById(3));
@@ -53,12 +55,15 @@ public class AccountService implements AccountServiceI, StandardizeService<Accou
     account.setPassword(this.hashPassword(account.getPassword()));
 
     Account newAccount = accountRepository.save(account);
-    if(newAccount == null)
+    if (newAccount == null) {
       return null;
+    }
 
-    ActivationCode activationCode = activationCodeService.addNew(newAccount, ActivationCode.TYPE_ACTIVATION_ACCOUNT_CODE);
-    if(activationCode == null)
+    ActivationCode activationCode = activationCodeService
+        .addNew(newAccount, ActivationCode.TYPE_ACTIVATION_ACCOUNT_CODE);
+    if (activationCode == null) {
       return null;
+    }
 
     List<ActivationCode> listActivationCodes = new ArrayList<>();
     listActivationCodes.add(activationCode);
@@ -68,31 +73,28 @@ public class AccountService implements AccountServiceI, StandardizeService<Accou
     return newAccount;
   }
 
-  private String hashPassword(String password){
+  private String hashPassword(String password) {
     PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
     return passwordEncoder.encode(password);
   }
 
-  private boolean validatePassword(String password){
-    if(password == null)
-      return false;
-
-    return password.length() <= 40 && password.length() >= 8;
-  }
-
   @Override
   public Account generateActivationCode(Account account, int type) {
-    List<ActivationCode> listToDelete = activationCodeRepository.getAllAddedAtLeastXHoursAgo(account.getId(), type, 2);
+    List<ActivationCode> listToDelete = activationCodeRepository
+        .getAllAddedAtLeastXHoursAgo(account.getId(), type, 2);
     activationCodeRepository.deleteAll(listToDelete);
 
-    List<ActivationCode> list = activationCodeRepository.getActivationCodesByAccountAndType(account, type);
-    if(list.size() > 3)
+    List<ActivationCode> list = activationCodeRepository
+        .getActivationCodesByAccountAndType(account, type);
+    if (list.size() > 3) {
       return null;
+    }
 
     ActivationCode activationCode = activationCodeService.addNew(account, type);
 
-    if(activationCode == null)
+    if (activationCode == null) {
       return null;
+    }
 
     List<ActivationCode> listActivationCodes = new ArrayList<>();
     listActivationCodes.add(activationCode);
@@ -102,46 +104,54 @@ public class AccountService implements AccountServiceI, StandardizeService<Accou
   }
 
   @Override
-  public boolean activateAccount(ActivationCode activationCode){
+  public boolean activateAccount(ActivationCode activationCode) {
     Account account = activationCode.getAccount();
     account.setEnabled(true);
 
     Account updatedAccount = accountRepository.save(account);
 
-    if(updatedAccount == null)
+    if (updatedAccount == null) {
       return false;
+    }
 
-    List<ActivationCode> activationCodeList = activationCodeRepository.getActivationCodesByAccountAndType(updatedAccount, ActivationCode.TYPE_ACTIVATION_ACCOUNT_CODE);
+    List<ActivationCode> activationCodeList = activationCodeRepository
+        .getActivationCodesByAccountAndType(updatedAccount,
+            ActivationCode.TYPE_ACTIVATION_ACCOUNT_CODE);
 
     activationCodeRepository.deleteAll(activationCodeList);
 
     return true;
   }
 
-
   @Override
-  public ActivationCode resetPasswordRequest(String activationCode){
-    if(!activationCodeService.validateActivationCode(activationCode))
+  public ActivationCode resetPasswordRequest(String activationCode) {
+    if (!activationCodeService.validateActivationCode(activationCode)) {
       return null;
+    }
 
-    ActivationCode activationCode1 = activationCodeRepository.getActivationCodeByCodeAndType(activationCode, ActivationCode.TYPE_RESET_PASSWORD_CODE);
+    ActivationCode activationCode1 = activationCodeRepository
+        .getActivationCodeByCodeAndType(activationCode, ActivationCode.TYPE_RESET_PASSWORD_CODE);
 
-    if(activationCode1 == null)
+    if (activationCode1 == null) {
       return null;
+    }
 
     return activationCode1;
   }
 
   @Override
-  public boolean resetPassword(ActivationCode activationCode, String newPassword){
+  public boolean resetPassword(ActivationCode activationCode, String newPassword) {
     Account account = activationCode.getAccount();
     account.setPassword(hashPassword(newPassword));
 
     Account updatedAccount = accountRepository.save(account);
-    if(updatedAccount == null)
+    if (updatedAccount == null) {
       return false;
+    }
 
-    List<ActivationCode> activationCodeList = activationCodeRepository.getActivationCodesByAccountAndType(updatedAccount, ActivationCode.TYPE_RESET_PASSWORD_CODE);
+    List<ActivationCode> activationCodeList = activationCodeRepository
+        .getActivationCodesByAccountAndType(updatedAccount,
+            ActivationCode.TYPE_RESET_PASSWORD_CODE);
 
     activationCodeRepository.deleteAll(activationCodeList);
 
@@ -149,22 +159,29 @@ public class AccountService implements AccountServiceI, StandardizeService<Accou
   }
 
   @Override
-  public boolean changeAccessRole(int accountId, String role){
-    if(accountId == Account.MAIN_ADMINISTRATOR_ID)
+  public boolean changeAccessRole(int accountId, String role) {
+    if (accountId == Account.MAIN_ADMINISTRATOR_ID) {
       return false;
+    }
 
-    Account loggedAccount = accountRepository.findAccountByUsername(SecurityContextHolder.getContext().getAuthentication().getName());
+    Account loggedAccount = accountRepository
+        .findAccountByUsername(SecurityContextHolder.getContext().getAuthentication().getName());
 
-    if(loggedAccount == null || (loggedAccount.getId() != Account.MAIN_ADMINISTRATOR_ID && role.equals(AccessRole.ROLE_ADMIN)))
+    if (loggedAccount == null || (loggedAccount.getId() != Account.MAIN_ADMINISTRATOR_ID && role
+        .equals(AccessRole.ROLE_ADMIN))) {
       return false; //cannot set ROLE_ADMIN if you are not main admin
+    }
 
     Account account = accountRepository.findAccountById(accountId);
-    if(account == null || (loggedAccount.getId() != Account.MAIN_ADMINISTRATOR_ID && account.getAccessRole().getRole().equals(AccessRole.ROLE_ADMIN)))
+    if (account == null || (loggedAccount.getId() != Account.MAIN_ADMINISTRATOR_ID && account
+        .getAccessRole().getRole().equals(AccessRole.ROLE_ADMIN))) {
       return false; //cannot change to other role for ADMIN if you are not a main admin
+    }
 
     AccessRole accessRole = accessRoleRepository.getAccessRoleByRole(role);
-    if(accessRole == null)
+    if (accessRole == null) {
       return false;
+    }
 
     account.setAccessRole(accessRole);
 
