295cbee48ecdead8276d916d14e56599e3f69696
Marcin <marcin7848@gmail.com>
2018-10-01 22:00:38 +0200

    change after reset system

--- src/main/java/com/goode/Language.java
+++ src/main/java/com/goode/Language.java
@@ -2,18 +2,7 @@ package com.goode;
 
 public enum Language {
 
-  REGISTRATION_GOODE("Rejestracja GoodE", "Registration GoodE"),
-  USERNAME_LENGTH_BETWEEN("Długość nazwy użytkownika musi być pomiędzy", "Username's length has to be between"),
-  USERNAME_LETTERS_NUMBERS_UNDERSCORE_ONLY("Nazwa użytkownika może zawierać tylko litery, cyfry i znak pokreślenia.",
-      "Username can contains letters, number and underscore only."),
   EMAIL_INCORRECT("Niepoprawny adres email.", "Incorrect email address."),
-  FILL_ALL_FIELDS("Uzupełnij wszystkie pola.", "Fill all fields."),
-  PASSWORD_LENGTH("Hasło musi mieć co najmniej 8 znaków.", "Password has to have 8 characters at least."),
-  REGISTER_NO_INCORRECT("Niepoprawny numer albumu.", "Incorrect register number."),
-  FIRSTNAME_LENGTH_BETWEEN("Długość imienia musi być pomiędzy", "First name's length has to be between"),
-  LASTNAME_LENGTH_BETWEEN("Długość nazwiska musi być pomiędzy", "Last name's length has to be between"),
-  FIRSTNAME_LETTERS_ONLY("Imię może zawierać tylko litery.", "First name can contains letters only."),
-  LASTNAME_LETTERS_ONLY("Nazwisko może zawierać tylko litery.", "Last name can contains letters only."),
   ACCOUNT_EXISTS("Konto z podanymi danymi już istnieje.", "Account with given data exists."),
   UNKNOWN_ERROR("Nieznany błąd. Spróbuj ponownie później.", "Unknown error. Try again later."),
   HELLO("Witaj", "Hello"),
